341ae9b514ba2fb3b9a9ae416baec34200fb98b0
Soham Mondal <soham.pf.mondal@gmail.com>
2019-08-01 17:29:28 +0530

    updated uri

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,28 @@
-import React, { Component  } from "react";
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-//Components 
-import BookList from './components/BookList';
-import AddBook from './components/AddBook';
-
+//Components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 //apollo client setup
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: "http://localhost:4000/graphql"
+});
 
-})
-
-class App extends Component { 
-
-    render(){
-        
-        return(
-          <ApolloProvider client={client}>
-            <div id="main">
-            <h1>My Reading List</h1>
-              <BookList />
-              <AddBook />
-            </div>
-          </ApolloProvider>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>My Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
