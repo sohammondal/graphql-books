c3327989f6b89cffad74a4832d2a9e315d276734
Soham Mondal <soham.pf.mondal@gmail.com>
2019-08-01 17:34:10 +0530

    checked NODE_ENV

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema = require("./schema/schema");
+const env = process.env.NODE_ENV;
+const schema =
+  env === "dev" ? require("./schema/dummySchema") : require("./schema/schema");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 
+// Setting Mongoose Debug based on Environment - PROD / DEV
+mongoose.set("debug", env === "dev");
+
 // allow cross origin requests
 app.use(cors());
 
 // Connect to Mongo
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true
-  }) // Adding new mongo url parser
-  .then(() => console.log("MONGODB CONNECTED..."))
-  .catch(err => console.log(err));
+try {
+  mongoose
+    .connect(
+      `mongodb://${process.env.DB_USER}:${
+        process.env.DB_PASS
+      }@ds243897.mlab.com:43897/${process.env.DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    ) // Adding new mongo url parser
+    .then(() =>
+      console.log("*************\tMONGODB CONNECTED\t\t*************")
+    );
+} catch (err) {
+  console.error(err.message);
+}
 
 app.use(
   "/graphql",
